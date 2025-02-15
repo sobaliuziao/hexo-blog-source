934e79b82b30574bfa057dd9200a157782a4ef42
sobaliuziao <liuziao20081122@163.com>
2025-02-15 18:54:31 +0800

    20250215

--- scripts/auto_open.js
+++ scripts/auto_open.js
@@ -7,5 +7,6 @@ var spawn = require('child_process').spawn;
 
 // Hexo 3
 hexo.on('new', function(data){
-  spawn('C:/Program Files/Typora/Typora.exe', [data.path]);
+  // spawn('C:/Program Files/Typora/Typora.exe', [data.path]);
+  spawn('C:/Users/liuziao/AppData/Local/Programs/Microsoft VS Code/Code.exe', [data.path]);
 });
